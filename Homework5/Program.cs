0147848d2ae1bbecb39277d1200767ca6d1fcfd3
Oleksandr75c <oleksandr.movchan75@gmail.com>
2023-06-05 19:09:43 +0300

    hw5

---
+++ Homework5/Program.cs
@@ -0,0 +1,40 @@
+﻿using Homework5;
+using System.ComponentModel;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+         Courses course1 = new Courses("C#1","Kristina","C# level1");
+         Courses course2 = new Courses("C#2", "Kristina", "C# level2");
+         Student stud1 = new Student("Oleksander", "Movchan", 47, "Zhitomyr");
+         Student stud2 = new Student("Vasya", "Pupkin", 35, "Kiev");
+
+            course1.Print();
+            course2.Print();
+            stud1.Print();
+            stud2.Print();
+
+            Console.WriteLine("\n*********** Add students to courses **********");
+            course1.AddOneStudents(ref stud1);
+            stud2.AddCource(ref course1);
+            stud2.AddCource(ref course2);
+
+            course1.Print();
+            course2.Print();
+            stud1.Print();
+            stud2.Print();
+
+            Console.WriteLine("\n************ del student from courses **********");
+            stud2.DelCource(ref course1);
+
+            course1.Print();
+            course2.Print();
+            stud1.Print();
+            stud2.Print();
+
+        }
+    }
+}
---
+++ Homework5/Student.cs
@@ -0,0 +1,52 @@
+﻿using Homework5;
+using System;
+
+public class Student
+{
+	public string firstName { get; set; }
+	public string lastName { get; set; }
+	public int age { get; set; }
+    public string city { get; set; }
+    internal Courses[] cources { get; set; } = new Courses[20];
+     public int counterCources { get; set; } = 0;
+   
+
+	public Student(string firstName, string lastName, int age, string city)    // konstruktor
+    { 
+        this.firstName = firstName; 
+        this.lastName = lastName;
+        this.age = age;
+        this.city = city;
+	}
+    public void Print()
+    {
+        Console.WriteLine("\n*** object Student ***");
+        Console.WriteLine($"Name: {firstName} {lastName} ");
+        Console.WriteLine($"City: {city}, Age: {age}");
+        Console.WriteLine("Active courses:");
+        for (int i = 0; i < this.counterCources;i++)
+        Console.WriteLine($" {1+i}. {this.cources[i].name}");
+    }
+
+    public void AddCource(ref Courses co)
+    {
+        co.totalStudents++;
+        this.cources[counterCources] = co;
+        this.counterCources++; 
+    }
+
+    public bool DelCource(ref Courses co)
+    {
+
+        for(int i=0; i< this.counterCources; i++)
+            if (co.name == this.cources[i].name) 
+            {
+                this.cources[i] = this.cources[this.counterCources - 1];
+                co.totalStudents--;
+                this.counterCources--;
+                return true;
+            }
+        return false;
+    }
+
+}
