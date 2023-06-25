9d9b36b09ab3360ea9f4e86f17f78a3ce61e7fdc
study_75@outlook.com <study_75@outlook.com>
2023-06-25 20:01:00 +0300

    additional8

---
+++ Additional8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lCity = new List<string> { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+            List<string> lfood = new List<string> { "Butter", "Honey", "Brade", "Biscuit" };
+            string[] arr1 = new string[3] { "Welcome", "to", "https://classroom.google" };
+
+
+            var lresult=lCity.OrderBy(x => x).OrderBy(x => x.Length);
+            foreach(var ele in lresult) Console.WriteLine(ele);
+
+            Console.WriteLine("\n");
+            lresult = lfood.OrderBy(x => x);
+            foreach (var ele in lresult)  Console.WriteLine(ele);
+
+            Console.WriteLine("\nEnter minimum length of the item");
+            int minl = Convert.ToInt32(Console.ReadLine());
+            var lresult1 = arr1.Where(x => x.Length > minl).Select(x => x);
+            foreach (var ele in lresult1) Console.WriteLine(ele);
+
+
+        }
+    }
+}
